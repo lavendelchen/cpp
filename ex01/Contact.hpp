d136050af85b19dc1cda16c11a6a39b41be4ba1c
lavendelchen <lavendel02sh@gmail.com>
2022-07-05 22:52:25 +0200

    started ex01

---
+++ ex01/Contact.hpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shaas <shaas@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 22:32:20 by shaas             #+#    #+#             */
+/*   Updated: 2022/07/05 22:50:58 by shaas            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <string>
+
+class Contact {
+	std::string firstName;
+	std::string firstName;
+	std::string firstName;
+	std::string firstName;
+	std::string firstName;
+	
+	private:
+		std::string firstName;
+		std::string firstName;
+		std::string firstName;
+}
+
+Hausaufgabenheft:
+	addSeite
+	addCover
+	removeCover
+	changeCover(NewCover)
+	Cover:
+		changeCover(CoverContent)
+		addSticker
+		InfoSticker:
+			addSticker
+			
+	Seiten:
+		changeSeite
+
+class PhoneBook {
+	Contact contacts[8];
+}
+
+Contact.cpp
+private:
+firstName
+
+public:
+setFirstName(name) {
+	if (name.hasNumbers)
+		return;
+	if (name.hasSpecialChars)
+		return;
+	name = firstName;
+}
+getFirstName() -> firsName
+
+
+
+main:
+Contact lars;
+lars.setFirstName(
+	"Lars"
+)
+std::cout << lars.getFirstName()
+
+#endif
