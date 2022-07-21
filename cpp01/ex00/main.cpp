1ffd1e308f227a6bcb00eff0ff95e4ff716538ac
lavendelchen <lavendel02sh@gmail.com>
2022-07-21 21:33:50 +0200

    makefile changed to make valgrind work

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: shaas <shaas@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 19:11:35 by shaas             #+#    #+#             */
-/*   Updated: 2022/07/21 19:23:53 by shaas            ###   ########.fr       */
+/*   Updated: 2022/07/21 21:25:10 by shaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	randomChump(std::string name);
 Zombie*	newZombie(std::string name);
 
-int main(void) {
+int main(void) { // why memleak??????
 	Zombie mike = Zombie("Mike");
 	mike.announce();
 
