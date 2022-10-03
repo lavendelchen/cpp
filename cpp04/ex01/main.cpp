997e048c957a20e28c3632c42ca031462eb72984
lavendelchen <lavendel02sh@gmail.com>
2022-10-03 20:25:28 +0200

    cpp04 ex01: added main from subject (not complete)

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,11 +6,12 @@
 /*   By: shaas <shaas@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 00:22:50 by shaas             #+#    #+#             */
-/*   Updated: 2022/10/01 23:55:33 by shaas            ###   ########.fr       */
+/*   Updated: 2022/10/03 19:32:59 by shaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <stdlib.h>
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
@@ -19,39 +20,9 @@
 #include "WrongClasses/WrongCat.hpp"
 
 int main() {
-	{
-		std::cout << "-----CORRECT USAGE-----\n\n";
-		Animal* meta = new Animal();
-		Animal* j = new Dog();
-		Animal* i = new Cat();
-	
-		std::cout << j->getType() << " " << std::endl;
-		std::cout << i->getType() << " " << std::endl;
-	
-		i->makeSound();
-		j->makeSound();
-		meta->makeSound();
-	
-		delete i;
-		delete j;
-		delete meta;
-	}
-	{
-		std::cout << "\n-----WRONG USAGE-----\n\n";
-		WrongAnimal* meta = new WrongAnimal();
-		WrongAnimal* j = new WrongDog();
-		WrongAnimal* i = new WrongCat();
-	
-		std::cout << j->getType() << " " << std::endl;
-		std::cout << i->getType() << " " << std::endl;
-	
-		i->makeSound();
-		j->makeSound();
-		meta->makeSound();
-	
-		delete i;
-		delete j;
-		delete meta;
-	}
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	delete j;//should not create a leak
+	delete i;
 	return 0;
 }
