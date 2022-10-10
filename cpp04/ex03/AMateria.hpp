2b87a32535440aa3ef62a7f1be68fc446ceba047
lavendelchen <lavendel02sh@gmail.com>
2022-10-10 17:32:11 +0200

    cpp04 ex03: added stuff to AMateria

--- cpp04/ex03/AMateria.hpp
+++ cpp04/ex03/AMateria.hpp
@@ -6,18 +6,25 @@
 /*   By: shaas <shaas@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 16:38:12 by shaas             #+#    #+#             */
-/*   Updated: 2022/10/10 16:38:40 by shaas            ###   ########.fr       */
+/*   Updated: 2022/10/10 17:01:05 by shaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
+
+# include <string>
 
 class AMateria
 {
 	protected:
-		[...]
+		std::string	type;
 	public:
-		AMateria(std::string const & type);
+		AMateria(std::string const &type);
 		[...]
-		std::string const & getType() const; //Returns the materia type
+		std::string const &getType() const; //Returns the materia type
 		virtual AMateria* clone() const = 0;
 		virtual void use(ICharacter& target);
 };
+
+#endif
