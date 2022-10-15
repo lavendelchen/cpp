40796b7baa88067923c00315fce7421eb56110bd
Lavendelchen <lavendel02sh@gmail.com>
2022-10-15 22:18:55 +0200

    cpp04 ex03: added Character class

---
+++ cpp04/ex03/Character.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Character.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shaas <shaas@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 22:15:51 by shaas             #+#    #+#             */
+/*   Updated: 2022/10/15 22:18:32 by shaas            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHARACTER_HPP
+# define CHARACTER_HPP
+
+# include <string>
+# include "ICharacter.hpp"
+
+class Character: virtual public ICharacter {
+	private:
+
+	public:
+}
+
+#endif
