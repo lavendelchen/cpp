3adb4bfeca198149fbe551638b94c5451460270d
lavendelchen <lavendel02sh@gmail.com>
2022-10-17 18:32:06 +0200

    cpp05 ex00: main created

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -6,29 +6,25 @@
 /*   By: shaas <shaas@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 00:02:02 by shaas             #+#    #+#             */
-/*   Updated: 2022/10/16 05:24:26 by shaas            ###   ########.fr       */
+/*   Updated: 2022/10/17 18:31:37 by shaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 /* ------------------------------- CONSTRUCTOR --------------------------------*/
-Bureaucrat::Bureaucrat(): name("Bureaucrat") {
-	std::cout << "Bureaucrat default constructor called" << std::endl;
-	grade = 1;
+Bureaucrat::Bureaucrat(): name("Bureaucrat"), grade(this->highestGrade) { }
+
+Bureaucrat::Bureaucrat(std::string name, int grade): name(name) {
+	if (grade < this->highestGrade || grade > this->lowestGrade)
+		this->grade = this->highestGrade;
+	else
+		this->grade = grade;
 }
 
-Bureaucrat::Bureaucrat(std::string name, int grade): name(name), grade(grade) {
-	std::cout << "Bureaucrat name constructor called" << std::endl;
-}
-
-Bureaucrat::Bureaucrat(const Bureaucrat &orig): name(orig.name) {
-	std::cout << "Bureaucrat copy constructor called" << std::endl;
-	this->grade = orig.grade;
-}
+Bureaucrat::Bureaucrat(const Bureaucrat &orig): name(orig.name), grade(orig.grade) { }
 
 Bureaucrat&	Bureaucrat::operator=(Bureaucrat const &rhs) {
-	std::cout << "Bureaucrat copy assignment operator called" << std::endl;
 	if (this != &rhs) {
 		this->grade = rhs.grade;
 	}
@@ -36,15 +32,13 @@ Bureaucrat&	Bureaucrat::operator=(Bureaucrat const &rhs) {
 }
 
 /* -------------------------------- DESTRUCTOR -------------------------------- */
-Bureaucrat::~Bureaucrat() {
-	std::cout << "Bureaucrat destructor called" << std::endl;
-}
+Bureaucrat::~Bureaucrat() { }
 
 /* --------------------------------- PUBLIC METHODS --------------------------------- */
 
 void	Bureaucrat::printAttributes(std::ostream &out) {
 	out	<< "Name: " << this->name 
-		<< "Grade: " << this->grade;
+		<< ", Grade: " << this->grade;
 }
 
 /* --------------------------------- OVERLOAD --------------------------------- */
--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: shaas <shaas@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 20:45:53 by shaas             #+#    #+#             */
-/*   Updated: 2022/10/16 05:13:14 by shaas            ###   ########.fr       */
+/*   Updated: 2022/10/17 16:43:30 by shaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,20 @@
 #include <string>
 
 int main(void) {
+	Bureaucrat	oscar;
+	Bureaucrat	angela("Angela", 34);
+	Bureaucrat	kevin(angela);
+	Bureaucrat	wrong("Wrong", -40);
+	Bureaucrat	wrong2("Wrong2", 1000);
+
+	std::cout << oscar << '\n';
+	std::cout << angela << '\n';
+	std::cout << kevin << '\n';
+	std::cout << wrong << '\n';
+	std::cout << wrong2 << '\n';
+
+	oscar = kevin;
+
+	std::cout << oscar << '\n';
+
 }
