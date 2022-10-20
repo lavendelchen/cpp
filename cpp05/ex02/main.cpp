d47686c1ccbaa640fd7157c61088b5c3615d06f5
lavendelchen <lavendel02sh@gmail.com>
2022-10-20 23:15:51 +0200

    cpp05 ex02: unnoetige tests removed

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: shaas <shaas@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 20:45:53 by shaas             #+#    #+#             */
-/*   Updated: 2022/10/20 23:12:47 by shaas            ###   ########.fr       */
+/*   Updated: 2022/10/20 23:14:18 by shaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,40 +42,4 @@ void	shrubberyCreationFormTests(void) {
 
 int main(void) {
 	shrubberyCreationFormTests();
-	// constructor & overload tests
-	// ShrubberyCreationForm	street;
-	// ShrubberyCreationForm	garden(true);
-	// ShrubberyCreationForm	lake(garden);
-
-	// std::cout << "-- Default constructor test -- \n" << street << "\n\n";
-	// std::cout << "-- Attribute constructor test -- \n" << garden << "\n\n";
-	// std::cout << "-- Copy constructor test -- \n" << lake << "\n\n";
-
-	// street = lake;
-
-	// std::cout << "-- Copy assignment operator test -- \n" << street << "\n\n";
-
-	// getter tests
-	// std::cout << "-- getName test -- " << garden.getName() << '\n';
-	// std::cout << "-- getSignStatus test -- " << garden.getSignStatus() << '\n';
-	// std::cout << "-- getSignGrade test -- " << garden.getSignGrade() << '\n';
-	// std::cout << "-- getExeGrade test -- " << garden.getExeGrade() << '\n';
-
-	//exception tests
-	// try {
-	// 	int	wrongSignGrade = -40;
-	// 	int	wrongExeGrade = 1000;
-	// 	std::cout << "\n-- Attribute constructor exception test (wrongGrades are modifiable) -- \n";
-	// 	ShrubberyCreationForm	wrong("Wrong", false, wrongSignGrade, wrongExeGrade);
-	// }
-	// catch (std::exception& e) {
-	// 	std::cout << e.what() << '\n';
-	// }
-
-	// int	signGrade = 100;
-	// std::cout << "\n-- signShrubberyCreationForm test (signGrade is modifiable) -- \n";
-	// ShrubberyCreationForm		f("A Very Important ShrubberyCreationForm", false, 75, 75);
-	// Bureaucrat	b("Some Guy", signGrade);
-	// b.signShrubberyCreationForm(f);
-	// std::cout << f << '\n';
 }
