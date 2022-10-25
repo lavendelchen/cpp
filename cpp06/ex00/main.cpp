9011c1bc330368872406536b986f9cecd982a050
lavendelchen <lavendel02sh@gmail.com>
2022-10-25 18:18:33 +0200

    cpp06 ex00 formatting change

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: shaas <shaas@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 20:45:53 by shaas             #+#    #+#             */
-/*   Updated: 2022/10/25 18:16:49 by shaas            ###   ########.fr       */
+/*   Updated: 2022/10/25 18:18:18 by shaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int main(int argnum, char *arg[]) {
 	try {
-	if (argnum != 2)
-		throw(std::runtime_error("Error: 1 argument needed"));
+		if (argnum != 2)
+			throw(std::runtime_error("Error: 1 argument needed"));
 
-	tTypes types;
+		tTypes types;
 
-	short argType = findArgType(arg[1], types);
-	convert(argType, types, arg);
-	print(argType, types);
+		short argType = findArgType(arg[1], types);
+		convert(argType, types, arg);
+		print(argType, types);
 	}
 	catch (std::exception &e) {
 		std::cout << e.what() << '\n';
